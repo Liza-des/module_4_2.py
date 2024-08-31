022bb9d767b89236497568116d47c5393f08c88f
Liza-des <lizades@bk.ru>
2024-08-31 07:57:17 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,11 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
+
+# Попытка вызвать inner_function вне функции test_function
+# inner_function()
